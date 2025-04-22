faa8e5fd48a74afea04ce244d817cf75344a82d1
Priya Muthaiya <mrpriya1994@gmail.com>
2025-04-22 11:38:07 +0100

    Practice1 commit

--- src/Collections/EX_ArrayList.java
+++ src/Collections/EX_ArrayList.java
@@ -8,7 +8,7 @@ public class EX_ArrayList {
         List al=new ArrayList();
         al.add(1);
         al.add(2);
-        al.add(3);
+        al.add(4);
 
         System.out.println(al.contains(5));
         System.out.println(al.indexOf(2));
